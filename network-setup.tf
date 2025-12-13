488da4172ce8a609db8e4b8aeae1fbe8ca701671
Mila Milstead <mnmilstead24@students.desu.edu>
2025-12-13 02:47:48 +0000

    Add storage configuration

--- network-setup.tf
+++ network-setup.tf
@@ -1,15 +1,15 @@
-# Azure Virtual Network Configuration
+ # Azure Storage Configuration
 
-resource "azurerm_virtual_network" "main" {
-   name                = "main-network"
-   address_space       = ["10.0.0.0/16"]
-   location            = "East US"
-   resource_group_name = "cloud-infrastructure-rg"
-}
+ resource "azurerm_storage_account" "main" {
+   name                     = "cloudinfrastorage"
+   resource_group_name      = "cloud-infrastructure-rg"
+   location                 = "East US"
+   account_tier             = "Standard"
+   account_replication_type = "LRS"
+ }
 
-resource "azurerm_subnet" "internal" {
-   name                 = "internal"
-   resource_group_name  = "cloud-infrastructure-rg"
-   virtual_network_name = azurerm_virtual_network.main.name
-   address_prefixes     = ["10.0.2.0/24"]
-}
+ resource "azurerm_storage_container" "data" {
+   name                  = "data"
+   storage_account_name  = azurerm_storage_account.main.name
+   container_access_type = "private"
+ }
